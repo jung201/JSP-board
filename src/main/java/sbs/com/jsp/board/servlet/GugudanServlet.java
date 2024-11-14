3b979a30cb105635800e84a8e48b9888c63034ea
junggyu <jungkyu201@naver.com>
2024-11-14 22:28:00 +0900

    1212

--- src/main/java/sbs/com/jsp/board/servlet/GugudanServlet.java
+++ src/main/java/sbs/com/jsp/board/servlet/GugudanServlet.java
@@ -14,7 +14,7 @@ public class GugudanServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 인식
         resp.setCharacterEncoding("UTF-8"); // 완성된 HTML의 인코딩을 UTF-8로 하겠다
-        resp.setContentType("text/html; charset utf-8"); // 브라우저에게 우리가 만든22 결과물이 UTF-8이다. 라고 알리는 의미
+        resp.setContentType("text/html; charset utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이다. 라고 알리는 의미
 
         int dan = Integer.parseInt(req.getParameter("dan"));
         int limit = Integer.parseInt(req.getParameter("limit"));
