7bed406cb670a27bc64a2fe3a36fe5fa1b67e1ec
junggyu <jungkyu201@naver.com>
2024-11-15 18:00:35 +0900

    dd

---
+++ src/main/java/sbs/com/jsp/board/Rq.java
@@ -0,0 +1,56 @@
+package sbs.com.jsp.board;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class Rq {
+    private final HttpServletRequest request;
+    private final HttpServletResponse response;
+
+    public Rq(HttpServletRequest request, HttpServletResponse response) {
+        this.request = request;
+        this.response = response;
+
+        try {
+            request.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 인식
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        response.setCharacterEncoding("UTF-8"); // 완성된 HTML의 인코딩을 UTF-8로 하겠다
+        response.setContentType("text/html; charset utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이다. 라고 알리는 의미
+    }
+
+    public int getIntParam(String paramName, int defaultValue) {
+        String value = request.getParameter(paramName);
+
+        if (value == null) {
+            return defaultValue;
+        }
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
+
+    }
+
+    public String getParam(String paramName, String defaultValue) {
+        String value = request.getParameter(paramName);
+
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public void writer(String str) {
+        try {
+            response.getWriter().append(str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/sbs/com/jsp/board/servlet/GugudanServlet.java
+++ src/main/java/sbs/com/jsp/board/servlet/GugudanServlet.java
@@ -5,24 +5,23 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import sbs.com.jsp.board.Rq;
 
 import java.io.IOException;
 
 @WebServlet("/gugudan")
 public class GugudanServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 인식
-        resp.setCharacterEncoding("UTF-8"); // 완성된 HTML의 인코딩을 UTF-8로 하겠다
-        resp.setContentType("text/html; charset utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이다. 라고 알리는 의미
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Rq rq = new Rq(request, response);
 
-        int dan = Integer.parseInt(req.getParameter("dan"));
-        int limit = Integer.parseInt(req.getParameter("limit"));
+        int dan = rq.getIntParam("dan", 9);
+        int limit = rq.getIntParam("limit", 9);
 
-        resp.getWriter().append("<h1>%d단</h1>".formatted(dan));
+        rq.writer("<h1>%d단</h1>".formatted(dan));
 
         for (int i = 1; i <= limit; i++) {
-            resp.getWriter().append("<div>%d * %d = %d</div>".formatted(dan, i, dan * i));
+            rq.writer(("<div>%d * %d = %d</div>".formatted(dan, i, dan * i)));
         }
 
     }
